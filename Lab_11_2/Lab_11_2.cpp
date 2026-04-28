c9d552bd0fb7f3a349f1ceb52eae2d88ac0e1bef
Maksym Baran <153942144+jacobstxt@users.noreply.github.com>
2026-04-28 12:54:05 +0300

    feat(lab-11): inherit constructors via using Base::Base

---
+++ Lab_11_2/Lab_11_2.cpp
@@ -0,0 +1,46 @@
+﻿#include <iostream>
+
+using namespace std;
+class Base
+{
+public:
+    Base()
+    {
+        cout << "Base()" << endl;
+    }
+    Base(const Base& other)
+    {
+        cout << "Base(Base&)" << endl;
+    }
+    explicit Base(int i) : num(i)
+    {
+        cout << "Base (int)" << endl;
+    }
+    explicit Base(char c) : letter(c)
+    {
+        cout << "Base(char)" << endl;
+    }
+private:
+    int num;
+    char letter;
+};
+class Derived : Base
+{
+public:
+    using Base::Base;
+private:
+    int newMember{ 0 };
+};
+
+int main()
+{
+    cout << "Derived d1(5) calls: ";
+    Derived d1(5);
+    cout << "Derived d1('c') calls: ";
+    Derived d2('c');
+    cout << "Derived d3 = d2 calls: ";
+    Derived d3 = d2;
+    cout << "Derived d4 calls: ";
+    Derived d4;
+}
+
