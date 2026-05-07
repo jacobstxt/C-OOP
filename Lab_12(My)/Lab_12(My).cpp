80f9d19647b522061eb35c81474e0b9ead5db58e
Maksym Baran <153942144+jacobstxt@users.noreply.github.com>
2026-05-07 17:58:51 +0300

    Lab_12 is done

---
+++ Lab_12(My)/Lab_12(My).cpp
@@ -0,0 +1,81 @@
+﻿#include <iostream>
+#include <cstring>
+#include <windows.h>
+using namespace std;
+
+class BankAccount {
+    string owner;       
+    double* history; 
+    int count;
+    int maxCount;
+
+public:
+    BankAccount(const string& name, int maxTransactions) {
+        owner = name;
+        history = new double[maxTransactions];
+		maxCount = maxTransactions;
+        count = 0;
+        cout << "[Constructor] Створено рахунок: " << owner << endl;
+    }
+
+    BankAccount(const BankAccount& a) {
+		owner = a.owner;
+        history = new double[a.maxCount];
+        for (int i = 0; i < a.count; i++)
+            history[i] = a.history[i];
+        count = a.count;
+        cout << "[Copy constructor] Скопійовано рахунок: " << owner << endl;
+    }
+
+    ~BankAccount() {
+        cout << "[Destructor] Видалено рахунок: " << owner << endl;
+        delete[] history;
+    }
+
+    void addTransaction(double amount) {
+        history[count++] = amount;
+    }
+
+    void setOwner(const string& name) {
+        owner = name;
+    }
+
+
+    void print() const {
+        cout << "Власник: " << owner << " | Транзакції: ";
+        for (int i = 0; i < count; i++)
+            cout << history[i] << " , ";
+        cout << endl;
+    }
+};
+
+int main() {  
+	SetConsoleCP(1251);
+	SetConsoleOutputCP(1251);
+
+    BankAccount acc1("Максим", 10);
+    acc1.addTransaction(1000.0);
+    acc1.addTransaction(-250.5);
+    acc1.addTransaction(500.0);
+
+    cout << "\n--- До копіювання ---" << endl;
+    acc1.print();
+
+
+    BankAccount acc2 = acc1;
+
+    cout << "\n--- Після копіювання ---" << endl;
+    acc1.print();
+    acc2.print();
+
+
+    acc1.setOwner("Андрій");
+    acc1.addTransaction(9999.0);
+
+    cout << "\n--- Після змін в acc1 ---" << endl;
+    acc1.print();
+    acc2.print();  
+
+    cout << endl;
+    return 0;
+}
---
+++ Lab_12.1/Lab_12.1.cpp
@@ -0,0 +1,60 @@
+﻿#include <iostream>
+using namespace std;
+
+
+class array1
+{
+    int* Arr_Ptr;
+    int size;
+public:
+    array1(int sz) 
+    {
+        Arr_Ptr = new int[sz];
+        if (!Arr_Ptr) exit(1);
+        size = sz;
+        cout << "Ordinary constructor" << endl;
+    }
+
+    array1(const array1& a) 
+    {
+        int i;
+        Arr_Ptr = new int[a.size]; 
+        if (!Arr_Ptr) exit(1);
+        for (i = 0; i < a.size; i++)
+            Arr_Ptr[i] = a.Arr_Ptr[i]; 
+        cout << "Copy constructor" << endl;
+    }
+
+    ~array1() { delete[] Arr_Ptr; }
+    void put(int i, int j)
+    {
+        if (i >= 0 && i < size)
+            Arr_Ptr[i] = j;
+        cout << "i= " << i << " Work put" << endl;
+    }
+    int get(int i)
+    {
+        return Arr_Ptr[i];
+    }
+};
+
+
+
+int main()
+{
+    array1 arr(10);
+    int i;
+    for (i = 0; i < 10; i++)
+        arr.put(i, i);
+
+    for (i = 0; i < 10; i++)
+        cout << arr.get(i);
+
+    cout << endl;
+    array1 new_arr = arr; 
+
+    for (i = 0; i < 10; i++)
+        cout << new_arr.get(i);
+    cout << endl;
+}
+
