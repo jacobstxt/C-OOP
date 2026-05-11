77eb82aaa0c5c54c2704a21634eb47f2f3aba10c
Maksym Baran <153942144+jacobstxt@users.noreply.github.com>
2026-05-11 14:07:05 +0300

    Start working on Lab_13

---
+++ Lab_13/Lab_13.cpp
@@ -0,0 +1,65 @@
+﻿#include <iostream>
+using namespace std;
+
+class CL1 
+{
+    int x;
+public:
+    CL1(int x1)  
+    {
+        x = x1;
+    }
+    void print()  
+    {
+        cout << "CL1::print()->" << endl;
+        cout << x << endl;
+    }
+
+    class CL2  
+    {
+        CL1& a;        
+        int y;  
+    public:
+        CL2(CL1& a1, int y1) : a(a1)
+        {
+            y = y1;
+        }
+        void print()  
+        {
+            cout << "CL1::CL2::print()->";
+            a.print();
+            cout << y << endl;
+        }
+
+        class CL3  
+        {
+            CL2& b;   
+            int z;     
+        public:
+            CL3(CL2& b1, int z1) : b(b1) 
+            {
+                z = z1;
+            }
+            void print()  
+            {
+                cout << "CL1::CL2::CL3::print()->";
+                b.print();
+                cout << z << endl;
+            }
+        }; 
+    };      
+};          
+
+int main()
+{
+    CL1 a(1); // a – об’єкт класу CL1
+    a.print(); // одержимо 1
+    CL1::CL2 b(a, 2); // b – об’єкт класу CL2
+    b.print(); // одержимо 1 2
+    CL1::CL2::CL3 c(b, 3); // c – об’єкт класу CL3
+    c.print(); // одержимо 1 2 3
+
+
+    return 0;
+}
+
